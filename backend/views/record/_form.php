9705be9301902847717293d99bb9031c187c71b2
rpbisbal <rizzabisbal@gmail.com>
2016-04-13 17:46:37 +0800

    backend record form

--- backend/views/record/_form.php
+++ backend/views/record/_form.php
@@ -2,7 +2,6 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Record */
@@ -14,25 +13,20 @@ use yii\helpers\ArrayHelper;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'employee_name')->textInput(['maxlength' => true]) ?>
-	
-	<?= $form->field($model, 'problem_id')->dropDownList(
-       ArrayHelper::map(\backend\models\Problem::find()->all(),'id', 'problem_type'),
-	   ['prompt'=>'Problem']
-    ) ?>
-	
-	<?= $form->field($model, 'short_description')->textInput(['maxlength' => true]) ?>
-	
-	<?= $form->field($model, 'employee_id')->dropDownList(
-       ArrayHelper::map(\backend\models\Employee::find()->all(),'id', 'cbs_firstname', 'cbs_lastname'),
-	   ['prompt'=>'IT in-charged']
-    ) ?>
 
     <?= $form->field($model, 'action_taken')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'date_recieved')->textInput() ?>
 
     <?= $form->field($model, 'remarks')->textInput(['maxlength' => true]) ?>
-    
+
+    <?= $form->field($model, 'employee_id')->textInput() ?>
+
+    <?= $form->field($model, 'short_description')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'problem_id')->textInput() ?>
+
+    <?= $form->field($model, 'category_id')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
