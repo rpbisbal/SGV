eaecac8027e58f9a23586c09cd1a6e6df4f9008c
Cherry Mae Buaquina <chemae0807@gmail.com>
2016-04-13 15:38:43 +0800

    added dropDownList for problem_id and employee_id

--- backend/views/record/_form.php
+++ backend/views/record/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use dosamigos\datepicker\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Record */
@@ -13,6 +15,18 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'employee_name')->textInput(['maxlength' => true]) ?>
+	
+	<?= $form->field($model, 'problem_id')->dropDownList(
+       ArrayHelper::map(\backend\models\Problem::find()->all(),'id', 'problem_type'),
+	   ['prompt'=>'Problem']
+    ) ?>
+	
+	<?= $form->field($model, 'short_description')->textInput(['maxlength' => true]) ?>
+	
+	<?= $form->field($model, 'employee_id')->dropDownList(
+       ArrayHelper::map(\backend\models\Employee::find()->all(),'id', 'cbs_firstname', 'cbs_lastname'),
+	   ['prompt'=>'IT in-charged']
+    ) ?>
 
     <?= $form->field($model, 'action_taken')->textInput(['maxlength' => true]) ?>
 
@@ -20,13 +34,18 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'remarks')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'employee_id')->textInput() ?>
+    
+	
+	
+	
 
-    <?= $form->field($model, 'short_description')->textInput(['maxlength' => true]) ?>
+    
 
-    <?= $form->field($model, 'recordcol')->textInput(['maxlength' => true]) ?>
+     <?= $form->field($model, 'recordcol')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'problem_id')->textInput() ?>
+    
+	
+	
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
